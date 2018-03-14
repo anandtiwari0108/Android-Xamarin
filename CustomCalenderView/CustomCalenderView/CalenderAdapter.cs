56787639a256574c76e17c1a67585a0b8414218b
anand tiwari <anand.t0108@gmail.com>
2018-03-14 19:10:28 +0530

    lastest with calendar

---
+++ CustomCalenderView/CustomCalenderView/CalenderAdapter.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Widget;
+using Android.Content;
+using Java.Util;
+using Android.Views;
+using Android.App;
+
+using Android.OS;
+using Android.Runtime;
+
+
+using Java.Lang;
+using Android.Graphics;
+
+namespace CustomCalenderView
+{
+    public class CalenderAdapter : BaseAdapter
+    {
+        int FIRST_DAY_OF_WEEK = 0; // Sunday = 0, Monday = 1
+        int prev;
+        private Context mContext;
+        int current;
+        private Calendar month;
+        private Calendar selectedDate;
+        private List<string> items;
+        public System.String[] days;
+
+        public CalenderAdapter(Context c, Calendar monthCalendar)
+        {
+            
+            month = monthCalendar;
+            selectedDate = (Calendar)monthCalendar.Clone();
+            mContext = c;
+            month.Set(CalendarField.DayOfMonth, 1);
+            this.items = new List<string>();
+            refreshDays();
+        }
+
+        public void refreshDays()
+        {
+            items.Clear();
+            
+            Calendar prevmonth =  (Calendar)month.Clone();
+            prevmonth.Set(CalendarField.Month, prevmonth.Get(CalendarField.Month) - 1);
+            int prevday = prevmonth.GetActualMaximum(CalendarField.DayOfMonth);
+            int lastDay = month.GetActualMaximum(CalendarField.DayOfMonth);
+            int firstDay = month.Get(CalendarField.DayOfWeek);
+            prev = firstDay - FIRST_DAY_OF_WEEK - 1;
+            // selecting the size of array
+            if (firstDay == 1)
+            {
+                days = new string[lastDay + (FIRST_DAY_OF_WEEK * 6)];
+            }
+            else
+            {
+                days = new System.String[lastDay + firstDay - (FIRST_DAY_OF_WEEK + 1)];
+            }
+            int j = FIRST_DAY_OF_WEEK;
+
+
+
+
+
+
+
+
+            // enter the empty value for the empty date for day
+            if (firstDay > 1)
+            {
+                var temp = firstDay - FIRST_DAY_OF_WEEK-1;
+                for (j = 0; j < firstDay - FIRST_DAY_OF_WEEK; j++)
+                {
+                    temp --;
+                    days[j] = (prevday-temp).ToString();
+                }
+            }
+            else
+            {
+                for (j = 0; j < FIRST_DAY_OF_WEEK * 6; j++)
+                {
+                    
+                    days[j] = "";
+                }
+                j = FIRST_DAY_OF_WEEK * 6 + 1; // sunday => 1, monday => 2
+            }
+
+
+
+
+
+            // enter the date from starting 
+            int dayNumber = 1;
+            for (int i = j - 1; i < days.Length; i++)
+            {
+                days[i] = "" + dayNumber;
+                dayNumber++;
+            }
+        }
+        public void setItems(List<string> items)
+        {
+            for (int i = 0; i != items.Count; i++)
+            {
+               
+                items[i]= "0" + items[i];
+                
+            }
+            this.items = items;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return days.Length;
+            }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            TextView dayView;
+            if (view == null) // no view to re-use, create new
+                view = LayoutInflater.From(mContext).Inflate(Resource.Layout.calender_item, null);
+
+            dayView = (TextView)view.FindViewById(Resource.Id.date);
+
+             
+            // disable empty days from the beginning
+            if (days[position].Equals(""))
+            {
+                dayView.Clickable = false;
+                dayView.Focusable = false;
+            }
+            else
+            {
+                
+                
+                if(prev>0 && prev>position)
+                {
+                    view.SetBackgroundResource(Resource.Color.lightcolor);
+                }
+                else
+                {
+                    if (month.Get(CalendarField.Year) == selectedDate.Get(CalendarField.Year) && month.Get(CalendarField.Month) == selectedDate.Get(CalendarField.Month) && days[position].Equals("" + selectedDate.Get(CalendarField.DayOfMonth)))
+                    {
+                        current = position;
+                        view.SetBackgroundColor(Android.Graphics.Color.Blue);
+                    }
+                    else
+                    {
+
+                        view.SetBackgroundResource(Resource.Color.holocolor);
+                    }
+                }
+               
+               
+            }
+
+            dayView.Text = days[position];
+            System.String date = days[position];
+
+            
+            return view;
+        }
+        
+
+    }
+}
---
+++ CustomCalenderView/CustomCalenderView/MainActivity.cs
@@ -0,0 +1,180 @@
+﻿using Android.App;
+
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+
+namespace CustomCalenderView
+{
+    [Activity(Label = "CustomCalenderView", MainLauncher = true,Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity, GestureDetector.IOnGestureListener
+    {
+        private static int SWIPE_THRESHOLD = 100;
+        private static int SWIPE_VELOCITY_THRESHOLD = 100;
+        
+        GestureDetector detector;
+        public Calendar month;  
+        public CalenderAdapter adapter;
+        public List<string> items;
+       
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            
+            var collapsingToolbar = FindViewById<CollapsingToolbarLayout>(Resource.Id.collapsing_toolbar);
+            collapsingToolbar.Title = "Calender";
+            month = Calendar.Instance;
+
+            
+            System.String date = DateTime.Now.Date.ToString("yyyy-MM-dd");
+        
+            char[] splitchar = { '-' };
+            System.String[] dateArr = date.Split(splitchar);
+
+
+
+           
+            month.Set(Int16.Parse(dateArr[0]), Int16.Parse(dateArr[1])-1, Int16.Parse(dateArr[2]));
+
+            items = new List<string>();
+            adapter = new CalenderAdapter(this, month);
+
+            detector = new GestureDetector(this);
+
+            GridView gridview = (GridView)FindViewById(Resource.Id.gridview);
+            gridview.Adapter = adapter;
+            gridview.ItemClick += grid_click;
+           
+            TextView title = (TextView)FindViewById(Resource.Id.title);
+            title.Text = Android.Text.Format.DateFormat.Format("MMMM yyyy", month);
+
+            TextView previous = (TextView)FindViewById(Resource.Id.previous);
+            previous.Click += previous_click;
+
+            TextView next = (TextView)FindViewById(Resource.Id.next);
+            next.Click += next_click;
+
+            
+        }
+        public void previous_click(object sender, EventArgs e)
+        {
+            previous_click1();
+        }
+
+        public void next_click(object sender, EventArgs e)
+        {
+            next_click1();
+        }
+        public void refreshCalendar()
+        {
+            TextView title = (TextView)FindViewById(Resource.Id.title);
+
+            adapter.refreshDays();
+            adapter.NotifyDataSetChanged();
+           
+            title.Text = Android.Text.Format.DateFormat.Format("MMMM yyyy", month);
+        }
+
+        public void grid_click(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            
+            TextView date = (TextView)e.View.FindViewById(Resource.Id.date);
+            System.String day = date.Text.ToString();
+            if(day.Equals(""))
+            {
+                return;
+            }
+            
+            Toast.MakeText(this, "Date --->" + Android.Text.Format.DateFormat.Format("yyyy-MM", month) + "-" + day, ToastLength.Short).Show();
+        }
+
+        public bool OnDown(MotionEvent e)
+        {
+            return true;
+        }
+
+        public bool OnFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+        {
+
+            float diffY = e2.GetY() - e1.GetY();
+            float diffX = e2.GetX() - e1.GetX();
+
+            if (Math.Abs(diffX) > Math.Abs(diffY))
+            {
+                if (Math.Abs(diffX) > SWIPE_THRESHOLD && Math.Abs(velocityX) > SWIPE_VELOCITY_THRESHOLD)
+                {
+                    if (diffX > 0)
+                    {
+                        previous_click1();
+                     }
+                    else
+                    {
+                        next_click1();
+                    }
+                }
+            }
+            return true;
+
+        }
+
+        private void previous_click1()
+        {
+            if (month.Get(CalendarField.Month) == month.GetActualMinimum(CalendarField.Month))
+            {
+                month.Set((month.Get(CalendarField.Year) - 1), month.GetActualMaximum(CalendarField.Month), 1);
+            }
+            else
+            {
+                month.Set(CalendarField.Month, month.Get(CalendarField.Month) - 1);
+            }
+            refreshCalendar();
+        }
+
+        private void next_click1()
+        {
+            if (month.Get(CalendarField.Month) == month.GetActualMaximum(CalendarField.Month))
+            {
+                month.Set((month.Get(CalendarField.Year) + 1), month.GetActualMinimum(CalendarField.Month), 1);
+            }
+            else
+            {
+                month.Set(CalendarField.Month, month.Get(CalendarField.Month) + 1);
+            }
+            refreshCalendar();
+        }
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            detector.OnTouchEvent(e);
+            return false;
+        }
+        public void OnLongPress(MotionEvent e)
+        {
+        }
+
+        public bool OnScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+        {
+            return false;
+        }
+
+        public void OnShowPress(MotionEvent e)
+        {
+            
+        }
+
+        public bool OnSingleTapUp(MotionEvent e)
+        {
+            return false;
+        }
+    }
+}
+
